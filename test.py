b2f90698cc1242e0d7c35acef48609b932518d0d
William McNeill <billmcn@gmail.com>
2014-08-14 17:40:49 -0500

    Test script

---
+++ test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import argparse
+import cPickle
+import gzip
+
+"""
+Use a model to print the error rate of labeled data
+"""
+
+parser = argparse.ArgumentParser(__doc__)
+parser.add_argument('model', help = 'Zipped model file')
+parser.add_argument('data', help = 'Pickled labeled data of the form (vectors, labels)')
+args = parser.parse_args()
+
+with gzip.open(args.model) as f:
+    model = cPickle.load(f)
+
+with gzip.open(args.data) as f:
+    x, y = cPickle.load(f)
+
+e = model.error_rate(y, x)
+print("Error rate %04f" % e)
