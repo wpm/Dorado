04797e71bdc29e3163bf37674332001ac6ab704a
William McNeill <billmcn@gmail.com>
2014-08-21 10:56:50 -0500

    Setuptools Packaging
    Use setuptools. Create entry points for scripts.

---
+++ dorado/scripts.py
@@ -0,0 +1,86 @@
+import argparse
+import dorado.train
+import logging
+import urllib
+
+
+def train():
+    """Train a classifier using stochastic gradient descent."""
+    args = dorado.model_training_arguments(train.__doc__)
+
+    logging.basicConfig(format='%(asctime)s %(message)s', 
+        level = getattr(logging, args.log.upper()))
+
+    iterations = dorado.train.sequential_iterations(
+        args.classifier, args.train, args.batch, args.rate)
+    model = dorado.train.train(
+        iterations, args.validation, args.min_epochs, args.max_epochs,
+        args.frequency, args.patience)[1]
+
+    dorado.dump_compressed(model, args.model)
+    logging.info("Created model %s" % args.model)
+    return 0
+
+
+def distributed_train():
+    """Train a classifier using stochastic gradient descent onx Spark."""
+    args = dorado.model_training_arguments(distributed_train.__doc__)
+
+    try:
+        from pyspark import SparkContext
+    except ImportError:
+        raise Exception("Run this with spark-submit")
+
+
+    logging.basicConfig(format='%(asctime)s %(message)s', 
+        level = getattr(logging, args.log.upper()))
+
+    sc = SparkContext("local", "Distributed Training")
+
+    iterations = dorado.train.parallel_iterations(
+        sc, args.classifier, args.train, args.batch, args.rate)
+    model = dorado.train.train(
+        iterations, args.validation, args.min_epochs, args.max_epochs,
+        args.frequency, args.patience)[1]
+
+    dorado.dump_compressed(model, args.model)
+    logging.info("Created model %s" % args.model)
+
+
+def test():
+    """Run a model on labeled data"""
+    parser = argparse.ArgumentParser(test.__doc__)
+    parser.add_argument('model', help = 'zipped model file')
+    parser.add_argument('data', help = 'labeled data')
+    args = parser.parse_args()
+
+    model = dorado.load_compressed(args.model)
+    data = dorado.load_compressed(args.data)
+
+    print("Error rate %04f" % model.error_rate(data.y, data.x))
+    return 0
+
+
+def download_mnist_digits():
+    """
+    Download the MNIST digit recognition data.
+
+    Write it to mnist.train.pkl.gz, mnist.test.pkl.gz, and mnist.valid.pkl.gz
+    files in the current directory.
+    """
+    parser = argparse.ArgumentParser(download_mnist_digits.__doc__)
+    parser.parse_args()
+
+    print('Download data')
+    origin = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+    filename = urllib.urlretrieve(origin)[0]
+
+    train_set, valid_set, test_set = dorado.load_compressed(filename)
+
+    for data, name  in zip([train_set, valid_set, test_set], ['train', 'valid', 'test']):
+        n = "mnist.%s.pkl.gz" % name
+        print("Write %s" % n)
+        x, y = data
+        dorado.dump_compressed(dorado.LabeledData(y, x), n)
+
+    return 0
--- setup.py
+++ setup.py
@@ -1,26 +1,37 @@
 import codecs
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with codecs.open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='Dorado',
-    version='0.1.0',
-    author='W.P. McNeill',
-    author_email='billmcn@gmail.com',
-    packages=['dorado'],
-    scripts=[
-        'bin/dorado_distributed_train.py', 
-        'bin/dorado_test', 'bin/dorado_train',
-        'bin/download_mnist_digits_dataset'],
-    url='https://github.com/wpm/Dorado',
-    description='Machine learning with Theano',
+    name = "Dorado",
+    version = "0.1.0",
+    packages = find_packages(),
+
+    entry_points = {
+        'console_scripts': [
+            'dorado_train = dorado.scripts:train',
+            'dorado_distributed_train = dorado.scripts:distributed_train',
+            'dorado_test = dorado.scripts:test',
+            'dorado_download_mnist_digits = dorado.scripts:download_mnist_digits'
+        ],
+    },
+
+    install_requires = [
+        'docutils>=0.3'
+        'theano',
+        'nltk'
+        ],
+
+    package_data = {
+        '': ['*.rst'],
+    },
+
+    author = "W.P. McNeill",
+    author_email = "billmcn@gmail.com",
+    description = "Distributed Machine learning with Theano",
+    keywords = "theano machine learning Spark",
+    url = "https://github.com/wpm/Dorado",
     long_description=long_description,
-    install_requires=[
-        "argparse",
-        "theano",
-        "nltk",
-        "numpy"
-    ],
 )
