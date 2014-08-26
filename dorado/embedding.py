d146f842caf849551021271760e4446431cb52f7
W.P. McNeill <billmcn@gmail.com>
2014-08-25 21:44:53 -0500

    Basic index and embedding

--- dorado/embedding.py
+++ dorado/embedding.py
@@ -3,40 +3,40 @@ import theano
 
 
 class Index(object):
-	"""
-	Mapping of objects to integer indexes
+    """
+    Mapping of objects to integer indexes
 
-	0 is reserved as an out-of-vocabularly index.
-	"""
-	def __init__(self, items = []):
-		self._indexes = {}
-		for item in items:
-			if not item in self._indexes:
-				self._indexes[item] = len(self)
+    0 is reserved as an out-of-vocabularly index.
+    """
 
-	def __repr__(self):
-		return "<Index %d items>" % len(self)
+    def __init__(self, items=[]):
+        self._indexes = {}
+        for item in items:
+            if not item in self._indexes:
+                self._indexes[item] = len(self)
 
-	def __len__(self):
-		return len(self._indexes) + 1
+    def __repr__(self):
+        return "<Index %d items>" % len(self)
 
-	def __getitem__(self, items):
-		return np.asarray([self._indexes.get(item, 0) for item in items])
+    def __len__(self):
+        return len(self._indexes) + 1
+
+    def __getitem__(self, items):
+        return np.asarray([self._indexes.get(item, 0) for item in items])
 
 
 class Embedding(object):
-	"""Distributed embedding of a set of objects"""
-	def __init__(self, index, m, b = 1):
-		self.index = index
-		self.m = m
-		self.C = theano.shared(
-			np.random.uniform(-b, b, (len(self.index), self.m)),
-			name = 'C')
+    """Distributed embedding of a set of objects"""
 
-	def __repr__(self):
-		return repr(self.C.get_value())
+    def __init__(self, index, m, b=1):
+        self.index = index
+        self.m = m
+        self.C = theano.shared(
+            np.random.uniform(-b, b, (len(self.index), self.m)),
+            name='C')
 
-	def __getitem__(self, items):
-		return self.C.get_value()[self.index[items]].flatten()
+    def __repr__(self):
+        return repr(self.C.get_value())
 
-# embeddings = self.C[X].reshape((X.shape[0], -1))
+    def __getitem__(self, items):
+        return self.C.get_value()[self.index[items]].flatten()
