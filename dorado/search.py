dd68b859f731cb2df1cf8c13f29cf68fff674016
W.P. McNeill <billmcn@gmail.com>
2014-11-27 08:47:17 -0600

    Rewrite search to create models on distributed machines

--- dorado/search.py
+++ dorado/search.py
@@ -8,11 +8,11 @@ class Search(object):
         self.learning_rate = learning_rate
         self.patience = patience
 
-    def epochs(self, model_factory, initial_parameters, training_batches):
+    def epochs(self, model_factory, initial_parameters, training_data):
         raise NotImplementedError()
 
     def _partitions(self, training_data):
-        return list(training_data.shuffle().partition(self.n))
+        return training_data.shuffle().partition(self.n)
 
 
 class SequentialSearch(Search):
@@ -26,26 +26,24 @@ class SequentialSearch(Search):
 
 
 class ParallelAveragedSearch(Search):
-    def epochs(self, model_factory, parameters, training_data):
-        training_batches = self._partitions(training_data)
-        models = self._create_models(n, model_factory, parameters)
+    def epochs(self, model_factory, initial_parameters, training_data):
+        ensemble = self._create_ensemble(model_factory, initial_parameters, training_data)
         while True:
-            models = [self._train_model(model, parameters, batch)
-                      for model, batch in self._enumerate_models(models, training_batches)]
-            parameters = reduce(operator.add, [model.parameters for model in models])
+            parameters = reduce(operator.add, [self._train_model(model, batch) for batch, model in ensemble])
             parameters /= self.n
             yield parameters
+            ensemble = [(batch, self._set_parameters(model, parameters)) for batch, model in ensemble]
 
-    def _create_models(self, n, model_type, initial_parameters):
-        return itertools.repeat(model_type(initial_parameters), n)
+    def _create_ensemble(self, model_factory, initial_parameters, training_data):
+        batches = self._partitions(training_data)
+        return zip(batches, itertools.repeat(model_factory(initial_parameters), self.n))
 
-    def _enumerate_models(self, models, training_batches):
-        for model, batch in itertools.izip(models, training_batches):
-            yield model, batch
+    def _train_model(self, model, data):
+        model.train(data, self.learning_rate)
+        return model.get_parameters()
 
-    def _train_model(self, model, parameters, batch):
+    def _set_parameters(self, model, parameters):
         model.set_parameters(parameters)
-        model.train(batch, self.learning_rate)
         return model
 
 
@@ -54,13 +52,13 @@ class SparkParallelAveragedSearch(ParallelAveragedSearch):
         self.spark_context = spark_context
         super(SparkParallelAveragedSearch, self).__init__(n, learning_rate, patience)
 
-    def epochs(self, model_factory, parameters, training_data):
-        training_batches = self._partitions(training_data)
-        zero = self.spark_context.broadcast(parameters.zero())
-        models = self._create_models(self.n, model_factory, parameters)
+    def epochs(self, model_factory, initial_parameters, training_data):
+        zero = self.spark_context.broadcast(initial_parameters.zero())
+        ensemble = self.spark_context.parallelize(
+            self._create_ensemble(model_factory, initial_parameters, training_data))
         while True:
-            models = self.spark_context.parallelize(self._enumerate_models(models, training_batches)). \
-                map(lambda model, batch: self._train_model(model, parameters, batch))
-            parameters = models.map(lambda model: model.parameters).fold(zero.value, operator.add)
+            parameters = ensemble.map(lambda batch, model:
+                                      self._train_model(model, batch)).fold(zero.value, operator.add)
             parameters /= self.n
             yield parameters
+            ensemble = ensemble.map(lambda batch, model: (batch, self._set_parameters(model, parameters)))
